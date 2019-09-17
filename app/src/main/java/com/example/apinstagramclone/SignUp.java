d1b2c608a9aec81d41752557722238be046c2ea5
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2019-09-17 18:03:29 +0530

    Parse Server and Fancy toast Library

--- app/src/main/java/com/example/apinstagramclone/SignUp.java
+++ app/src/main/java/com/example/apinstagramclone/SignUp.java
@@ -1,46 +1,92 @@
 package com.example.apinstagramclone;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.parse.GetCallback;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
-public class SignUp extends AppCompatActivity {
-
-    private TextView txtGetData;
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    private Button butSaveObject;
+    private EditText etName, etPunchSpeed, etPunchPower, etKickSpeed, etKickPower;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txtGetData = findViewById(R.id.txtDataServer);
+        setContentView(R.layout.activity_signup);
 
-        txtGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignUp.this, "Text Area Is clicked", Toast.LENGTH_SHORT).show();
-                ParseQuery<ParseObject> parseQuary = ParseQuery.getQuery("Boxer");
-                parseQuary.getInBackground("Lu6HZ4okHA", new GetCallback<ParseObject>() {
-                    @Override
-                    public void done(ParseObject object, ParseException e) {
-                        if(object != null && e == null){
+        butSaveObject = findViewById(R.id.butSaveObject);
+        etName = findViewById(R.id.etName);
+        etPunchSpeed = findViewById(R.id.etPunchSpeed);
+        etPunchPower = findViewById(R.id.etPunchPower);
+        etKickSpeed = findViewById(R.id.etKickSpeed);
+        etKickPower = findViewById(R.id.etKickPower);
 
-                            txtGetData.setText(object.get("punch_speed").toString());
-                        }else{
-                            Toast.makeText(SignUp.this, e.toString(), Toast.LENGTH_SHORT).show();
-                            Log.i("Instagram",e.toString());
-                        }
+        butSaveObject.setOnClickListener(SignUp.this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        try {
+            final ParseObject kickBoxer = new ParseObject("KickBoxer");
+            kickBoxer.put("name", etName.getText().toString());
+            kickBoxer.put("punchSpeed", Integer.parseInt(etPunchSpeed.getText().toString()));
+            kickBoxer.put("punchPower", Integer.parseInt(etPunchPower.getText().toString()));
+            kickBoxer.put("kickSpeed", Integer.parseInt(etKickSpeed.getText().toString()));
+            kickBoxer.put("kickPower", Integer.parseInt(etKickPower.getText().toString()));
+            kickBoxer.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(SignUp.this, kickBoxer.get("name") + " is saved to server.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUp.this, kickBoxer.get("name") + " is now save." + e.toString(), Toast.LENGTH_SHORT).show();
+                        Log.i("InstaError", e.toString());
                     }
-                });
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    // Function to save data to the server
+    public void helloWorldTrapped(View view) {
+//        ParseObject boxer = new ParseObject("Boxer");
+//        boxer.put("punch_speed", 200);
+//        boxer.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e == null){
+//                    Toast.makeText(SignUp.this, "Boxer Object is Saved successfully", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        final ParseObject kickBoxer = new ParseObject("KickBoxer");
+        kickBoxer.put("name", "Tanshu");
+        kickBoxer.put("punchSpeed", 1000);
+        kickBoxer.put("punchPower", 2000);
+        kickBoxer.put("kickSpeed", 3000);
+        kickBoxer.put("kickPower", 4000);
+        kickBoxer.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SignUp.this, kickBoxer.get("name") + "  is Saved to server.", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/apinstagramclone/activity/SignUpLoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.apinstagramclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apinstagramclone.R;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_login);
+
+    }
+}
