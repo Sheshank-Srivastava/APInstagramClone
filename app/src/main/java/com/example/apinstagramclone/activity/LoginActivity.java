97a07184feb07090c7493e7ab9b7035f93dea39b
Sheshank Srivastava <shesh.sri@zvky.com>
2020-01-16 15:18:02 +0530

    Setup and Login is set

--- app/src/main/java/com/example/apinstagramclone/activity/LoginActivity.java
+++ app/src/main/java/com/example/apinstagramclone/activity/LoginActivity.java
@@ -2,15 +2,75 @@ package com.example.apinstagramclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.apinstagramclone.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity
+                            implements View.OnClickListener{
+    Button btn_Login,btn_SignUp;
+    EditText et_Email,et_Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_Login = findViewById(R.id.btnLogIn_LogInActivity);
+        btn_SignUp = findViewById(R.id.btnSignUp_LogInActivity);
+
+        et_Email = findViewById(R.id.etEmailLogIn);
+        et_Password = findViewById(R.id.etPasswordLogIn);
+
+        btn_Login.setOnClickListener(this);
+        btn_SignUp.setOnClickListener(this);
+
+        et_Password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == EditorInfo.IME_ACTION_DONE && event.getAction()== KeyEvent.ACTION_DOWN){
+                    onClick(btn_Login);
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.btnLogIn_LogInActivity:
+
+                ParseUser.logInInBackground(et_Email.getText().toString(),
+                        et_Password.getText().toString(), new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+                                if(user==null&& e!=null){
+                                    Toast.makeText(LoginActivity.this, "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+                                Toast.makeText(LoginActivity.this, et_Email.getText().toString()+" is logged in.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                break;
+            case R.id.btnSignUp_LogInActivity:
+                startActivity(new Intent(LoginActivity.this,SignUp.class));
+                finish();
+                break;
+            default:
+
+        }
     }
 }
--- app/src/main/java/com/example/apinstagramclone/activity/SignUp.java
+++ app/src/main/java/com/example/apinstagramclone/activity/SignUp.java
@@ -1,18 +1,22 @@
 package com.example.apinstagramclone.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.apinstagramclone.R;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import org.jetbrains.annotations.Nullable;
+
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,7 +37,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         btn_SignUp.setOnClickListener(this);
         btn_LogIn.setOnClickListener(this);
 
-        if(ParseUser.getCurrentUser()!=null){
+        if (ParseUser.getCurrentUser() != null) {
             ParseUser.getCurrentUser().logOut();
         }
 
@@ -43,10 +47,22 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnSignUp:
+
+                if (et_Email.getText().toString().equals("") ||
+                        et_UserName.getText().toString().equals("") ||
+                        et_Password.getText().toString().equals("")) {
+                    Toast.makeText(this, "Email,UserName,Password is  required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 final ParseUser appUser = new ParseUser();
                 appUser.setEmail(et_Email.getText().toString().trim());
                 appUser.setUsername(et_UserName.getText().toString().trim());
                 appUser.setPassword(et_Password.getText().toString().trim());
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("SigningUp");
+                progressDialog.setMessage("Signing up in Progress");
+                progressDialog.show();
+
                 appUser.signUpInBackground(new SignUpCallback() {
                     @Override
                     public void done(ParseException e) {
@@ -56,10 +72,13 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                             Toast.makeText(SignUp.this, "There is an error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
 
                         }
+                        progressDialog.dismiss();
                     }
                 });
                 break;
             case R.id.btnLogIn:
+                startActivity(new Intent(SignUp.this, LoginActivity.class));
+                finish();
                 break;
         }
     }
